51e284d59e8f97fca4bc26a02b614ca1bf2ad8ba
slashedtires <kenneth.m.piekarski@gmail.com>
2017-03-30 18:42:39 -0700

    Fixed footer alt typo

--- footer.php
+++ footer.php
@@ -16,22 +16,22 @@
   <div class="row">
 
     <div id="social-footer" class="social-icon-zone col-xs-12">
-      <a href="https://www.facebook.com/megabogmusic/" target="_blank" alt="OFF TEMPO on Facebook">
+      <a href="https://www.facebook.com/megabogmusic/" target="_blank" alt="Mega Bog on Facebook">
         <i id="soc-f" class="fa fa-facebook social-icons"></i>
       </a>
-      <a href="https://soundcloud.com/erin-elizabeth-b" target="_blank" alt="OFF TEMPO on SoundCloud">
+      <a href="https://soundcloud.com/erin-elizabeth-b" target="_blank" alt="Mega Bog on SoundCloud">
         <i id="soc-sc" class="fa fa-soundcloud social-icons"></i>
       </a>
-      <a href="https://megabog.bandcamp.com/" target="_blank" alt="OFF TEMPO on YouTube">
+      <a href="https://megabog.bandcamp.com/" target="_blank" alt="Mega Bog on Bandcamp">
         <i id="soc-bc" class="fa fa-bandcamp social-icons"></i>
       </a>
-      <a href="http://www.instagram.com/mega_bog" target="_blank" alt="OFF TEMPO on Instagram">
+      <a href="http://www.instagram.com/mega_bog" target="_blank" alt="Mega Bog on Instagram">
         <i id="soc-in" class="fa fa-instagram social-icons"></i>
       </a>
-      <a href="http://www.twitter.com/mega_bog" target="_blank" alt="OFF TEMPO on Twitter">
+      <a href="http://www.twitter.com/mega_bog" target="_blank" alt="Mega Bog on Twitter">
         <i id="soc-tw" class="fa fa-twitter social-icons"></i>
       </a>
-      <a href="https://play.spotify.com/artist/0zodxoaogKBIOzB0c0zFUO" target="_blank" alt="OFF TEMPO on Twitter">
+      <a href="https://play.spotify.com/artist/0zodxoaogKBIOzB0c0zFUO" target="_blank" alt="Mega Bog on Spotify">
         <i id="soc-spot" class="fa fa-spotify social-icons"></i>
       </a>
     </div>
